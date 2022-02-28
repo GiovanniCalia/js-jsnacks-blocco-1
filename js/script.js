eaa4b75b2cd2dd3f237f6103dcf8cb6846f93222
GiovanniCalia <johnca@outlook.it>
2022-02-28 16:19:06 +0100

    Completamento snack 1.9

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // SNACK 1
 
 
-/*const num1 = parseInt(prompt("numero 1"));
+/*
+const num1 = parseInt(prompt("numero 1"));
 const num2 = parseInt(prompt("numero 2"));
 
 if (num1 > num2){
@@ -10,13 +11,14 @@ if (num1 > num2){
     alert(num2)
 } else{
     alert("sono uguali")
-}*/
+}
+*/
 
 
 // SNACK 2
 
-
-/*let parola = prompt("parola 1");
+/*
+let parola = prompt("parola 1");
 let parola2 = prompt("parola 2");
 let risultato = document.getElementById("stampa")
 
@@ -27,13 +29,15 @@ if (parola.length > parola2.length){
     risultato.innerHTML =`${parola2} <br> ${parola}`;
 } else{
     risultato.innerHTML = `Uguali`;
-}*/
+}
+*/
 
 
 // SNACK 3
 
 
-/*let arrNum = [];
+/*
+let arrNum = [];
 let risultato = document.getElementById("stampa");
 
 for (i = 0; i < 10; i++ ){
@@ -47,13 +51,15 @@ for (i = 0; i < arrNum.length; i++ ){
     somma = somma + arrNum[i];
 }
 
-risultato.innerHTML = `La somma è: ${somma}`*/
+risultato.innerHTML = `La somma è: ${somma}`;
+*/
 
 
 // SNACK 4
 
 
-/*const nome = prompt("Salve, qual è il suo nome?");
+/*
+const nome = prompt("Salve, qual è il suo nome?");
 const nomiInvitati = ["Giovanni", "Jessica", "Mauro", "Gabriel", "Nicoleta"];
 
 let presente = false;
@@ -69,13 +75,15 @@ if (presente){
     risultato.innerHTML = `Benvenuto/a, ${nome}!`;
 } else{
     risultato.innerHTML = `Spiacenti, il suo nome non è nella lista, ${nome}`;
-}*/
+}
+*/
 
 
 // SNACK 5
 
 
-/*let arrNum = [];
+/*
+let arrNum = [];
 
 for (i = 0; i < 6; i++ ){
     let userNum = parseInt(prompt("inserisci un numero"));
@@ -84,12 +92,14 @@ for (i = 0; i < 6; i++ ){
         arrNum.push(userNum)
         console.log(arrNum)
     } 
-}*/
+}
+*/
 
 
 // SNACK 6
 
 
+/*
 let somma = 0;
 let risultato = document.getElementById("stampa");
 
@@ -99,4 +109,75 @@ userNumber = userNumber.toString();
 for( let currentNumber = 0; currentNumber < userNumber.length; currentNumber++){
     risultato.innerHTML = ` La somma è: ${somma += parseInt(userNumber[currentNumber])}`;
 }
+*/
+
+
+
+// SECONDA PARTE ESESCIZIO
+
+
+
+//SNACK 1.9
+
+
+// METODO POCO EFFICENTE
+
+/*
+let arrNum = [1,2,3,4,5,6,7,8,9,10];
+let somma = 0;
+
+for (i = 0; i < arrNum.length; i++ ){
+    somma = somma + arrNum[i];
+}
+
+console.log(somma);
+
+function ArrayAvg(arrNum) {
+    let i = 0, somma = 0, ArrayLen = arrNum.length;
+    while (i < ArrayLen) {
+        somma = somma + arrNum[i++];
+}
+    return somma / ArrayLen;
+}
+
+let a = ArrayAvg(arrNum);
+console.log(a);
+*/
+
+
+// METODO EFFICENTE CON ARRAY
+
+/*
+let arrNum = [];
+let somma = 0;
+
+for (let i = 1; i <= 10; i++ ){
+    somma = somma + i;
+    arrNum.push(somma[i]);
+}
+
+console.log(somma);
+
+
+let media = somma / 10;
+console.log(media);
+*/
+
+
+// METODO EFFICENTE SENZA ARRAY
+
+/*
+let somma = 0;
+
+for (let numero = 1; numero <= 10; numero++){
+    somma += numero;
+    console.log(numero);
+}
+
+let media = somma / 10;
+console.log(media);
+console.log(somma);
+*/
+
+// SNACK 2.1
 
